76eb5ca302d00e4888356dfa0064d1e7ba2adb60
sacarvy <sukhpreetben10@gmail.com>
2022-01-04 08:07:46 +0530

    updated the astro buold options

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,10 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	devOptions: {},
+	buildOptions: {
+		site: "http://selfv4.netlify.app", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+		sitemap: true, // Generate sitemap (set to "false" to disable)
+	},
 	// Enable the Svelte renderer to support Svelte components.
 	renderers: ["@astrojs/renderer-svelte"],
 });
